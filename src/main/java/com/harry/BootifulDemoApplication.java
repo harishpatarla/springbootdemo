dee88aaf8cbf8556fbdf1f618d2519645449655c
Harish Patharla <Harish.Patharla@ig.com>
2016-11-12 17:15:56 +0530

    intial commit

---
+++ src/main/java/com/harry/BootifulDemoApplication.java
@@ -0,0 +1,12 @@
+package com.harry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootifulDemoApplication.class, args);
+    }
+}
