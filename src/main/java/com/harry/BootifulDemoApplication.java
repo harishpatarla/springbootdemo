4cda870877f282261d7a35be37c6a244db3fd241
Harish Patharla <Harish.Patharla@ig.com>
2016-11-13 18:43:55 +0530

    added conditional example

--- src/main/java/com/harry/BootifulDemoApplication.java
+++ src/main/java/com/harry/BootifulDemoApplication.java
@@ -2,11 +2,21 @@ package com.harry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BootifulDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BootifulDemoApplication.class, args);
+    }
+
+    @ConditionalOnClass(String.class)
+    @Bean
+    public String string() {
+        String s = "conditionals work...";
+        System.out.println(s);
+        return s;
     }
 }
