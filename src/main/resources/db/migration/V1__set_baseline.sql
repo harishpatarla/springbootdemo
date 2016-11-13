04431dc392f82614a33244e927bf871011b391b7
Harish Patharla <Harish.Patharla@ig.com>
2016-11-13 13:32:17 +0530

    removed data.sql, added flyway and oracle dependency, added datasource connection properties, added flyway migration scripts

---
+++ src/main/resources/db/migration/V1__set_baseline.sql
@@ -0,0 +1,5 @@
+-- first migration file empty due to rebase on migration. This is for flyway to ignore any tables/triggers/objects pre-flyway integration.
+-- flyway.baseline-on-migrate --> property to tell Whether to automatically call baseline when migrate is executed against a non-empty schema with no metadata table.
+-- flyway.baseline-on-migrate=true is set in runtime properties for this to work for an existing schema
+
+
---
+++ src/main/resources/db/migration/V2__person_ddl_dml.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PERSON (
+  id    INT PRIMARY KEY,
+  name  VARCHAR(255),
+  email VARCHAR(255)
+);
+
+INSERT INTO PERSON (id, name, email) VALUES
+  (1, 'Tom', 'tom@gmail.com');
